4ea798a1bd2f7b6eb458549d69f99484fa2ccfb5
Precioussoul <habsoff@gmail.com>
2023-12-09 15:46:58 +0100

    feat: Abstract render and user Events

--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -2,9 +2,12 @@ import { render as renderComponent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 
+export const render = (ui: React.ReactElement, options: any) => {
+  const user = userEvent.setup();
+  const result = renderComponent(ui, options);
 
-export const render = (ui: React.ReactElement, options: any) {
-    const user = userEvent.setup()
-    
-}
-
+  return {
+    ...result,
+    user,
+  };
+};
